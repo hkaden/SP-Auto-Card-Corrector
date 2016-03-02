cc2e6b792d1ce14271724d978330b5deb80ace00
l2003201 <davidlui@Macintosh-2.local>
2016-03-02 11:58:33 +0800

    base function done

--- corrector.py
+++ corrector.py
@@ -9,9 +9,24 @@ db = MySQLdb.connect(host="localhost", user="root", passwd="", db="spgame", unix
 cursor = db.cursor()
 
 # 執行 SQL 語句
-cursor.execute("SELECT * FROM `users` WHERE `usr_water` < 0 OR `usr_fire` < 0 OR `usr_earth` < 0 OR `usr_wind` < 0")
+cursor.execute("SELECT `usr_id`, `usr_name`, usr_water, usr_fire, usr_earth, usr_wind FROM `users` WHERE usr_water < 0 OR usr_fire < 0 OR usr_earth < 0 OR usr_wind < 0")
 result = cursor.fetchall()
 
 # 輸出結果
 for record in result:
-    print record
+    if record[2] < 0 :
+        cursor.execute("UPDATE users SET usr_water = '0' WHERE usr_id = " + str(record[0]))
+        db.commit()
+        print "[LOG] ID:" + str(record[0]) + ". " + "Name:" + str(record[1]) + ". water white card abnormal, but this fixed"
+    elif record[3] < 0 :
+        cursor.execute("UPDATE users SET usr_fire = '0' WHERE usr_id = " + str(record[0]))
+        db.commit()
+        print "[LOG] ID:" + str(record[0]) + ". " + "Name:" + str(record[1]) + ". fire white card abnormal, but this fixed"
+    elif record[4] < 0 :
+        cursor.execute("UPDATE users SET usr_earth = '0' WHERE usr_id = " + str(record[0]))
+        db.commit()
+        print "[LOG] ID:" + str(record[0]) + ". " + "Name:" + str(record[1]) + ". earth white card abnormal, but this fixed"
+    elif record[5] < 0 :
+        cursor.execute("UPDATE users SET usr_wind = '0' WHERE usr_id = " + str(record[0]))
+        db.commit()
+        print "[LOG] ID:" + str(record[0]) + ". " + "Name:" + str(record[1]) + ". wind white card abnormal, but this fixed"
